4b8260d61d7860536974d0cff1f8e979fe3be760
Michael Bachmann <mbachmann@bbmsoft.net>
2023-12-25 15:55:26 +0100

    install script sets up hotspot

--- install.sh
+++ install.sh
@@ -29,6 +29,54 @@ function installScoreboard() {
     sudo docker run --name scoreboard -d --restart always -p 80:80 -v "$HOME/.config/scoreboard/config.js":"/html/__config__.js" babymotte/scoreboard:1.0.0
 }
 
+function setupHotSpot() {
+
+    # Install required packages
+    sudo apt-get install -y hostapd dnsmasq --fix-missing
+    sudo systemctl unmask hostapd
+    sudo systemctl unmask dnsmasq
+    sudo systemctl enable hostapd
+    sudo systemctl enable dnsmasq
+    sudo systemctl start hostapd
+    sudo systemctl start dnsmasq
+
+    # Configure static IP address
+    echo "interface wlan0
+    static ip_address=192.168.0.10/24
+    nohook wpa_supplicant
+    denyinterfaces eth0
+    denyinterfaces wlan0" | sudo tee -a /etc/dhcpcd.conf
+
+    # Configure DHCP
+    sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+    echo "interface=wlan0
+  dhcp-range=192.168.0.11,192.168.0.100,255.255.255.0,24h" | sudo tee /etc/dnsmasq.conf
+
+    # Configure hotspot
+    echo 'country_code=DE
+interface=wlan0
+hw_mode=g
+channel=9
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP CCMP
+rsn_pairwise=CCMP
+ssid=ScoreBoard
+wpa_passphrase=volleyball' | sudo tee /etc/hostapd/hostapd.conf
+    echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' | sudo tee /etc/default/hostapd
+    echo '127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+
+192.168.0.10       scoreboard
+192.168.0.10       scoreboard.local' | sudo tee /etc/hosts
+}
+
 function setupAutostart() {
     sudo apt-get install -y chromium xterm fonts-noto-color-emoji matchbox-window-manager xautomation unclutter --fix-missing
     echo '#!/bin/sh
@@ -53,7 +101,11 @@ installDocker
 echo "Installing scoreboard …"
 installScoreboard
 
+echo "Setting up Wifi hotspot …"
+setupHotSpot
+
 echo "Setting up autostart …"
 setupAutostart
 
 echo "Setup complete. For correct autostart behavior, run 'sudo raspi-config' and under 'System Options' set 'Boot / Auto Login' mode to 'Console Autologin' and reboot!"
+echo "After reboot, log into Wifi 'ScoreBoard' (password is '!Score!Board!') and navigate to 'http://scoreboard.local'"
